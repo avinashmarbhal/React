927a5ed7fee421722f122a873e8cfd297d307ddb
Avinash M <avinashmarabhal7@gmail.com>
2025-02-03 23:03:53 +0530

    bug fix

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -1,47 +1,45 @@
 // chnage the config in the tailwindConfig darkMode:"class"
 
-
-import { useEffect, useState } from 'react'
-import './App.css'
-import { ThemeProvider } from './contexts/theme.js'
-import ThemeBtn from './componants/ThemeBtn.jsx'
-import Card from './componants/Card.jsx'
-import tailwindConfig from '../tailwind.config.js'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./contexts/theme.js";
+import ThemeBtn from "./componants/ThemeBtn.jsx";
+import Card from "./componants/Card.jsx";
+import tailwindConfig from "../tailwind.config.js";
 
 function App() {
-  const [themeMode, setThemeMode] = useState("light")
+  const [themeMode, setThemeMode] = useState("light");
 
   const lightTheme = () => {
-    setThemeMode("light")
-  }
+    setThemeMode("light");
+  };
 
   const darkTheme = () => {
-    setThemeMode("dark")
-  }
+    setThemeMode("dark");
+  };
 
   // actual change in theme
 
   useEffect(() => {
-    document.querySelector('html').classList.remove("light", "dark")
-    document.querySelector('html').classList.add(themeMode)
-  }, [themeMode])
-  
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
-          <div className="w-full">
-              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                  <ThemeBtn />
-              </div>
-
-              <div className="w-full max-w-sm mx-auto">
-                  <Card />
-              </div>
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
           </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
       </div>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
